da6a5057cf4abd635236b60b124199061e2f7c8d
MRusinova <rusinovamihaela@gmail.com>
2023-03-10 11:20:23 +0000

    Create setUp and tearDown methods

--- test_student.py
+++ test_student.py
@@ -1,27 +1,36 @@
 import unittest
 from student import Student
 
+
 class TestStudent(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
+
+    def setUp(self):
+        print("setup")
+        self.student = Student("John", "Doe")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tear down Class")
+
+    def tearDown(self):
+        print("tear down")
 
     def test_full_name(self):
-        student = Student('John', 'Doe')
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
 
-        self.assertEqual(student.full_name, 'John Doe')
-
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+        self.assertTrue(self.student.naughty_list)
 
     def test_email(self):
-        student = Student("John", "Doe")
+        print("test_email")
+        self.assertEqual(self.student.email, "john.doe@email.com")
 
-        self.assertEqual(student.email, 'john.doe@email.com')
-
-
-    def test_alerrt_santa(self):
-        student = Student("John", "Doe")
-        student.alert_santa()
-
-        self.assertTrue(student.naughty_list)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-
